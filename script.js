68c64ab0874cf1bc7ee6cd3a5b1cf4000456cf8e
fgeratovic03 <154012279+fgeratovic03@users.noreply.github.com>
2024-11-23 02:40:23 +0100

    vraceno staro

--- script.js
+++ script.js
@@ -74,6 +74,9 @@ function prikaziProizvode() {
     }
 }
 
+function sortirajProizvode() {
+    var selectElement = document.getElementById("kriterijum-sortiranja");
+    var kriterijum = selectElement.value;
 
     var sortirajFunkcija;
     if (kriterijum === "ime") {
